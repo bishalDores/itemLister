fff39ec2bf9b5f53bb50bb0205f206dc3e435037
bishalpd <bishal@gmail.com>
2018-10-23 16:09:25 +0600

    code commenting done

--- main.js
+++ main.js
@@ -9,12 +9,19 @@ filter.addEventListener('keyup', filterItem);
 
 function addNewItem(e) {
     e.preventDefault();
+    //getting the value of the new item from the input field
     var newItem = document.getElementById('item').value;
+    //creating new li item
     var li = document.createElement('li');
+    //creating new button element
     var delBtn = document.createElement('button');
+    //giving class name to the li item
     li.className = "list-group-item";
+    //giving class name to delete button
     delBtn.className = "btn btn-danger btn-sm float-right delete";
+    //connecting li with text
     li.appendChild(document.createTextNode(newItem));
+    //putting cross icon to the delete button
     delBtn.appendChild(document.createTextNode('X'));
     li.appendChild(delBtn);
     itemList.appendChild(li);
@@ -22,7 +29,9 @@ function addNewItem(e) {
 
 function removeItem(e) {
     if (e.target.classList.contains('delete')) {
+        //confirm is for the popup
         if (confirm('Are you sure?')) {
+            //parent element used to get the list item
             var li = e.target.parentElement;
             itemList.removeChild(li);
         }
@@ -33,9 +42,11 @@ function filterItem(e){
     var text = e.target.value.toLowerCase();
 //    console.log(text);
     var items = itemList.getElementsByTagName('li');
+    //loop through the array of items containing li
     Array.from(items).forEach(function(item){
         var itemName = item.firstChild.textContent;
         //console.log(itemName);
+        // converting all text inputs into lower case
         if(itemName.toLowerCase().indexOf(text)!=-1){
            item.style.display="block";
            }else{
